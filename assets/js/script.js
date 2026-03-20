76d2c71fdba0fd8a3fbb35c956da08a90f0ec7ff
Infamousmick <mmarchese6690@gmail.com>
2026-03-20 20:53:13 +0100

    animating playBtn, better Card placement

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,26 +42,61 @@ const createURL = () => {
 createURL();
 
 const createCards = (song, serachedAuthor) => {
-  const songTitle = song.title;
-  const title = document.createElement("h2");
-  title.textContent = songTitle;
-
-  const artist = song.artist.name;
-  const artistP = document.createElement("p");
-  artistP.textContent = artist;
-
-  const albumCover = song.album.cover_xl;
-  const image = document.createElement("img");
-  image.src = albumCover;
-
-  const songDuration = parseDuration(song.duration);
-  console.log(songDuration);
-
   const sectionId = `${serachedAuthor.toLowerCase()}Section`;
   const authorSection = document.getElementById(sectionId);
 
+  const col = document.createElement("div");
+  col.setAttribute("class", "col mb-4");
+
+  const card = document.createElement("div");
+  card.setAttribute("class", "card h-100 text-white border-0 p-2");
+
+  const imageContainer = document.createElement("div");
+  imageContainer.setAttribute("class", "imgContainer");
+
+  const albumCover = song.album.cover_xl;
+  const image = document.createElement("img");
+  image.setAttribute("class", " shadow-sm w-100");
+  image.src = albumCover;
+
+  const playButton = document.createElement("i");
+  playButton.setAttribute(
+    "class",
+    "playBtn fa-solid fa-play bg-success rounded-circle d-flex justify-content-center align-items-center shadow",
+  );
+  setupPlayButton(playButton);
+
+  const cardBody = document.createElement("div");
+  cardBody.setAttribute("class", "card-body px-0 py-2");
+
+  const songTopContainer = document.createElement("div");
+  songTopContainer.setAttribute(
+    "class",
+    "d-flex justify-content-between align-items-center gap-2 mb-1 flex-nowrap",
+  );
+
+  const songTitle = song.title;
+  const title = document.createElement("h5");
+  title.setAttribute("class", "card-title text-truncate fw-bold mb-0");
+  title.textContent = songTitle;
+
+  const songDuration = document.createElement("p");
+  songDuration.setAttribute("class", "card-text mb-0 text-light fw-bold");
+  songDuration.textContent = parseDuration(song.duration);
+
+  const artistName = song.artist.name;
+  const artist = document.createElement("p");
+  artist.setAttribute("class", "card-text small text-secondary");
+  artist.textContent = artistName;
+
+  songTopContainer.append(title, songDuration);
+  cardBody.append(songTopContainer, artist);
+  imageContainer.append(image, playButton);
+  card.append(imageContainer, cardBody);
+  col.appendChild(card);
+
   if (authorSection) {
-    authorSection.append(title, artistP, songDuration, image);
+    authorSection.append(col);
     authorSection.parentNode.classList.remove("d-none");
   } else {
     console.warn(
@@ -79,4 +114,11 @@ const parseDuration = (time) => {
     ":" +
     seconds.toString().padStart(2, "0");
   return timeString;
+};
+
+const setupPlayButton = (btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("fa-play");
+    btn.classList.toggle("fa-pause");
+  });
 };
